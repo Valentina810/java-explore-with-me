582f2e1a14bdc372ad7367c61438bde07bbcb225
Valentina Kolesnikova <83814517+Valentina810@users.noreply.github.com>
2023-04-25 12:16:03 +0300

    Merge pull request #7
    * feat: оптимизировать работу с Compilation

--- ewm-service/src/main/java/ru/practicum/compilation/dao/CompilationRepository.java
+++ ewm-service/src/main/java/ru/practicum/compilation/dao/CompilationRepository.java
@@ -6,10 +6,32 @@ import org.springframework.data.jpa.repository.Query;
 import ru.practicum.compilation.model.Compilation;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CompilationRepository extends JpaRepository<Compilation, Long> {
-	@Query(value = "from Compilation cm " +
-			"join fetch cm.events ev " +
+	@Query(value = "from Compilation cm "+
+			"left join fetch cm.events ev " +
+			"left join fetch ev.initiator " +
+			"left join fetch ev.state " +
+			"left join fetch ev.location " +
+			"left join fetch ev.category " +
 			"where cm.pinned=:pinned")
 	List<Compilation> findByPinned(boolean pinned, PageRequest pageRequest);
+
+	@Query(value = "from Compilation cm "+
+			"left join fetch cm.events ev " +
+			"left join fetch ev.initiator " +
+			"left join fetch ev.state " +
+			"left join fetch ev.location " +
+			"left join fetch ev.category ")
+	List<Compilation> findAll(PageRequest pageRequest);
+
+	@Query(value = "from Compilation cm "+
+			"left join fetch cm.events ev " +
+			"left join fetch ev.initiator " +
+			"left join fetch ev.state " +
+			"left join fetch ev.location " +
+			"left join fetch ev.category "+
+			"where cm.id=:compId")
+	Optional<Compilation> findById(long compId);
 }
